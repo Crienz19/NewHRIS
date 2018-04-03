583aca5d2228f4c09f066766cb4d8d0ebfb70f1a
Renz Mergenio <zner.mergenio@gmail.com>
2018-04-03 10:24:12 -0800

    Position Field: from select to input

--- resources/views/pages/employee/my-profile.blade.php
+++ resources/views/pages/employee/my-profile.blade.php
@@ -114,7 +114,7 @@
                         </div>
                         <input type="date" class="form-control" id="birthdate-field" name="birthdate-field">
                         <select name="department-field" id="department-field" class="form-control"></select><br>
-                        <select name="position-field" id="position-field" class="form-control"></select>
+                        <input type="text" name="position-field" class="form-control"></input>
                         <select name="branch-field" id="branch-field" class="form-control">
                             <option value="" selected>Select Branch</option>
                             <option value="MANILA">MANILA</option>
