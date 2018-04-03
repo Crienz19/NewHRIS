c250615b78e2943c233fd7036585ceaecdb257d2
Renz Mergenio <zner.mergenio@gmail.com>
2018-04-03 09:20:08 -0800

    Profile Picture Width and Length => 250px

--- resources/views/pages/employee/employee-profile.blade.php
+++ resources/views/pages/employee/employee-profile.blade.php
@@ -49,7 +49,7 @@
                                         <div class="row">
                                             <div class="col-md-4">
                                                 <a href="#" class="thumbnail">
-                                                    <img width="150px" length="150px" id="photo" src="">
+                                                    <img width="250px" length="250px" id="photo" src="">
                                                 </a>
                                             </div>
                                             <div class="col-md-4">
