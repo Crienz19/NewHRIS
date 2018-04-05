87722d5463e5f857e7a6de2cd13025f6288325a6
Renz Mergenio <zner.mergenio@gmail.com>
2018-04-05 13:57:35 -0800

    OT Filter

--- app/Http/Controllers/SupervisorController.php
+++ app/Http/Controllers/SupervisorController.php
@@ -117,7 +117,7 @@ class SupervisorController extends Controller
                          ->join('departments', 'departments.id', '=', 'employees.department_id')
                          ->join('branches', 'branches.id', '=', 'employees.branch_id')
                          ->select(['employees.full_name as employee', 'position_id as position', 'departments.name as department', 'branches.name as branch', 'overtimes.*'])
-                         ->where('departments.supervisor', $user->id)
+                         ->where('departments.supervisor', $user->user_id)
                          ->where('employees.branch_id', $user->branch_id)
                          ->where('overtimes.status', $status)
                          ->whereRoleIs('user')
