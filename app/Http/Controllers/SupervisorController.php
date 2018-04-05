df414b4b22e82261383f75b5fa9bae1be840ed5c
Renz Mergenio <zner.mergenio@gmail.com>
2018-04-05 10:53:18 -0800

    Department Filter

--- app/Http/Controllers/SupervisorController.php
+++ app/Http/Controllers/SupervisorController.php
@@ -45,7 +45,7 @@ class SupervisorController extends Controller
                       ->join('departments', 'departments.id', '=', 'employees.department_id')
                       ->join('branches', 'branches.id', '=', 'employees.branch_id')
                       ->select(['employees.full_name as employee', 'positions.name as position', 'departments.name as department', 'branches.name as branch', 'leaves.*'])
-                      ->where('departments.supervisor', Auth::user()->id)
+                      ->where('departments.id', $user->department_id)
                       ->where('employees.branch_id', $user->branch_id)
                       ->where('leaves.recommending_approval', $status)
                       ->whereRoleIs('user')
