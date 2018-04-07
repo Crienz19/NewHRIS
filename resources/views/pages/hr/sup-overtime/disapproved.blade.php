e8aa6377ac4ed8599cc5eeaeb874b324d8a77b92
Renz Mergenio <zner.mergenio@gmail.com>
2018-04-06 17:12:47 -0800

    Supervisor Overtime 500 entries

--- resources/views/pages/hr/sup-overtime/disapproved.blade.php
+++ resources/views/pages/hr/sup-overtime/disapproved.blade.php
@@ -85,6 +85,7 @@
                 'processing': true,
                 'serverSide': true,
                 'ajax': '{{ url("/hr/otRequests") }}/Disapproved/supervisor',
+                "iDisplayLength": 500,
                 'dom': '<"pull-right m-b-5"B><"#filter.pull-left"><t><p>',
                 'buttons': [
                     {
